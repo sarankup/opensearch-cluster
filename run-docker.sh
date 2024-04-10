6bfa13382289dab8fdb78783e89a63a7f63ebeee
Saravanan Kuppusamy <saran@newgen.co>
2024-04-10 14:25:39 +0530

    single and multiple cluster'

--- run-docker.sh
+++ run-docker.sh
@@ -1,2 +1,4 @@
 export OPENSEARCH_INITIAL_ADMIN_PASSWORD=opensearch-1996N2009S
 docker-compose -f docker-compose-node1.yml up
+docker-compose -f docker-compose-node2.yml up
+docker-compose -f docker-compose-node3.yml up
